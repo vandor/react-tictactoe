d0eda22d810e0d21c66781c91f44e764a125c73a
Josh Loya <vandor@users.noreply.github.com>
2018-09-24 12:08:50 -0400

    Detect when game is a draw

--- src/index.js
+++ src/index.js
@@ -99,6 +99,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (current.squares.every(x => x)) {
+      status = "It's a draw!";
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
