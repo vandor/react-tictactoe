626b7fe744a84cfd6234ba2f373b4eaa83c436d2
Josh Loya <vandor@users.noreply.github.com>
2018-09-24 12:48:26 -0400

    Pull components into their own files

---
+++ src/Board.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Square from './Square';
+
+export default class Board extends React.Component {
+  renderSquare(i, key) {
+    return (
+      <Square
+        key={key}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const rowSize = 3,
+      colSize = rowSize,
+      rows = [...Array(rowSize).keys()],
+      cols = [...Array(colSize).keys()];
+
+    return (
+      <div>
+        {rows.map(row => (
+          <div className="board-row" key={row}>
+            {cols.map(col => (
+              this.renderSquare((rowSize * row) + col, col)
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
